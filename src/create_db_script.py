8c7b38a33d19ca52512a5b70fda7685e9596880d
MatanTalvi10 <matantalbi@gmail.com>
2024-03-07 19:02:30 +0200

    tables

--- src/create_db_script.py
+++ src/create_db_script.py
@@ -23,52 +23,51 @@ def main():
 
   # Creating movies table
   TABLES['movies'] = (
-      "CREATE TABLE IF NOT EXISTS 'movies' ("
-      "   'movie_id' INT NOT NULL,"
-      "   'title' VARCHAR(50) NOT NULL,"
-      "   'release_date' DATE,"
-      "   'run_time' INT NOT NULL,"
-      "   'adult_only' VARCHAR(50) NOT NULL,"
-      "   PRIMARY KEY ('movie_id')"
+      "CREATE TABLE IF NOT EXISTS movies ("
+      "   movie_id INT NOT NULL,"
+      "   title VARCHAR(50) NOT NULL,"
+      "   release_date DATE,"
+      "   run_time INT NOT NULL,"
+      "   adult_only VARCHAR(50) NOT NULL,"
+      "   PRIMARY KEY (movie_id)"
       ") ENGINE=InnoDB")
-
 
   # Creating genres table
   TABLES['genres'] = (
-      "CREATE TABLE IF NOT EXISTS 'genres' ("
-      "   'genre_id' VARCHAR(50) NOT NULL,"
-      "   'genre_name' varchar(50) NOT NULL,"
-      "   PRIMARY KEY ('genre_id')"
+      "CREATE TABLE IF NOT EXISTS genres ("
+      "   genre_id VARCHAR(50) NOT NULL,"
+      "   genre_name varchar(50) NOT NULL,"
+      "   PRIMARY KEY (genre_id)"
       ") ENGINE=InnoDB")
-
+  
   # Creating ratings table
   TABLES['ratings'] = (
-      "CREATE TABLE IF NOT EXISTS 'ratings' ("
-      "   'movie_id' VARCHAR(50) NOT NULL,"
-      "   'user_id' varchar(50) NOT NULL,"
-      "   'rating' FLOAT,"
-      "   PRIMARY KEY ('user_id','movie_id'),"
-      "   FOREIGN KEY ('movie_id') REFERENCES 'movies' ('movie_id')"
+      "CREATE TABLE IF NOT EXISTS ratings ("
+      "   movie_id VARCHAR(50) NOT NULL,"
+      "   user_id varchar(50) NOT NULL,"
+      "   rating FLOAT,"
+      "   PRIMARY KEY (user_id,movie_id),"
+      "   FOREIGN KEY (movie_id) REFERENCES movies (movie_id)"
       ") ENGINE=InnoDB")
 
 
   # Creating budget table
   TABLES['budget'] = (
-      "CREATE TABLE IF NOT EXISTS 'budget' ("
-      "   'movie_id' INT NOT NULL,"
-      "   'budget' INT NOT NULL,"
-      "   PRIMARY KEY ('movie_id'),"
-      "   FOREIGN KEY ('movie_id') REFERENCES 'movies' ('movie_id')"
+      "CREATE TABLE IF NOT EXISTS budget ("
+      "   movie_id INT NOT NULL,"
+      "   budget INT NOT NULL,"
+      "   PRIMARY KEY (movie_id),"
+      "   FOREIGN KEY (movie_id) REFERENCES movies (movie_id)"
       ") ENGINE=InnoDB")
 
   # Creating genre_movie table
   TABLES['genre_movie'] = (
-      "CREATE TABLE IF NOT EXISTS 'genre_movie' ("
-      "   'movie_id' INT NOT NULL,"
-      "   'genre_id' INT NOT NULL,"
-      "   PRIMARY KEY ('movie_id','genre_id),"
-      "   FOREIGN KEY ('movie_id') REFERENCES 'movies' ('movie_id'),"
-      "   FOREIGN KEY ('genre_id') REFERENCES 'genres' ('genre_id')"
+      "CREATE TABLE IF NOT EXISTS genre_movie ("
+      "   movie_id INT NOT NULL,"
+      "   genre_id INT NOT NULL,"
+      "   PRIMARY KEY (movie_id,genre_id),"
+      "   FOREIGN KEY (movie_id) REFERENCES movies (movie_id),"
+      "   FOREIGN KEY (genre_id) REFERENCES genres (genre_id)"
       ") ENGINE=InnoDB")
 
 
