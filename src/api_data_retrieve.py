160ad53e719c9a06396797181e97ca0c7602735f
MatanTalvi10 <matantalbi@gmail.com>
2024-03-10 20:13:19 +0200

    DELETED BUDGET

--- src/api_data_retrieve.py
+++ src/api_data_retrieve.py
@@ -10,9 +10,9 @@ from datetime import datetime
 
 
 def main():
-    create_db_script.main()
-    read_and_insert('movies',add_movies)
-    #read_and_insert('budget',add_budget)
+    #create_db_script.main()
+    #read_and_insert('movies',add_movies)
+    read_and_insert('budget',add_budget)
     #read_and_insert('genres',add_genres)
     #read_and_insert('genre_movie',add_genre_movie)
     #read_and_insert('ratings',add_ratings)
