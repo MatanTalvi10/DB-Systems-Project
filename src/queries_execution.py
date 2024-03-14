984387bcc4a35ffc450f8caf0e37427014a5eb69
MatanTalvi10 <matantalbi@gmail.com>
2024-03-14 12:51:01 +0200

    HI

--- src/queries_execution.py
+++ src/queries_execution.py
@@ -2,45 +2,54 @@ import queries_db_script as qdb
 
 
 # 1   
-qdb.query_1('Love')
+#example_1 = qdb.query_1('Love')
 '''
 This query returns all the columns of the movies table but only 
 for movies with the word 'Love' in their title
 '''
 
 # 2   
-qdb.query_1('King')
+#example_2 = qdb.query_1('King')
 '''
 This query returns all the columns of the movies table but only 
 for movies with the word 'king' in their title
 '''
 
 # 3   
-qdb.query_2()
+#example_3 = qdb.query_2()
 '''
 Counts the number of movies in each genre according to predetermined buckets of budget.
 '''
 
 # 4   
-qdb.query_3()
+example_4 = qdb.query_3()
 '''
 Lists the movies and their genre with their average rating by all users.
 '''
 
 # 5   
-qdb.query_4(1)
+#example_5 = qdb.query_4(1)
 '''
 return the names of the movies user_id = 1 rated and the ratings for each movie.
 '''
 
-# 5   
-qdb.query_4(2)
+# 6   
+#example_6 = qdb.query_4(2)
 '''
 return the names of the movies user_id = 1 rated and the ratings for each movie.
 '''
 
-# 5   
-qdb.query_5(2)
+# 7   
+#example_7 = qdb.query_5(2)
 '''
 return the names of the movies user_id = 1 rated and the ratings for each movie.
 '''
+
+# 8   
+#example_8 = qdb.query_6(2008)
+'''
+return all the movies that where released in 2008 and add its budget,
+order by budget .
+'''
+
+
